ff9eda160a4350769cec663d4ab80aeb8a412171
Sowiii <149089033+Sowiii@users.noreply.github.com>
2025-06-20 12:48:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -147,5 +147,8 @@ def decrypt():
         return jsonify({"success": False, "error": str(e)})
 
 if __name__ == "__main__":
+    import os
     generate_key()
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
